e836922f62ae4fe635e8f9be9307e047814373d1
avinashpitti <avinash87129@gmail.com>
2026-03-27 13:08:15 +0530

    basic variable declaration

---
+++ variables/variables.js
@@ -0,0 +1,17 @@
+let a=12;
+let b=41;
+console.log(a+b);
+
+// let a=23; you can't redeclare with the same variable by using let
+
+// But you can reassign the variable with let
+a=19
+console.log(a+b);
+
+
+const c=7;
+const d=8;
+console.log(c+d);
+
+// c=10; neither can redeclare nor can assign the value with const
+console.log(c+d);
